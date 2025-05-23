77200a75c35f32255a5347ff1cb8e0cbc1fe4ba7
Gergő Orosz <oroszg55@gmail.com>
2025-05-24 00:05:51 +0200

    Create database schema

---
+++ database/migrations/2025_05_23_212020_create_vehicles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('passenger_capacity');
+            $table->float('range');
+            $table->foreignId('fuel_type_id')->constrained('fuel_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
